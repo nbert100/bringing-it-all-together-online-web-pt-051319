58be2c89e8caab4d44b60d1e7b3ad5a0a0bdcc51
Nia Bert <nbert14@gmail.com>
2019-07-28 15:45:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,5 +50,10 @@ class Dog
     dog
   end
   
-  
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    DB[:conn].execute(sql, id).map do |row|
+       self.new_from_db(row)
+     end.first
+  end
 end
